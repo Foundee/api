841107c0346ff21916b6c68fbc7d34af98556980
Arkadiusz Waluk <arkadiusz@waluk.pl>
2018-01-28 15:50:19 +0100

    Add API docs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,9 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Swagger\Annotations as SWG;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * Default test method
+     *
+     * @Route("/", methods={"GET"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="API work well"
+     * )
+     * @SWG\Tag(name="default")
+     */
     public function index(): Response
     {
         return $this->json([]);
