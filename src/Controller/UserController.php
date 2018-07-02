a85dfa66aa5a1867fdef7b9fb01d4805979b5abf
Mateusz <nowygracz01@gmail.com>
2018-07-02 16:28:02 +0200

    Fix

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,13 +9,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Swagger\Annotations as SWG;
-<<<<<<< HEAD
 use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-=======
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
 
 class UserController extends Controller
 {
@@ -68,7 +65,6 @@ class UserController extends Controller
     }
 
     /**
-<<<<<<< HEAD
      * @param Request $request
      * @param TranslatorInterface $translation
      * @param ValidatorInterface $validator
@@ -81,32 +77,19 @@ class UserController extends Controller
         TranslatorInterface $translation,
         ValidatorInterface $validator
     ): JsonResponse
-=======
-     * Check if email is registered.
-     */
-    public function checkEmail(Request $request): JsonResponse
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
     {
         $email = $request->query->get('email');
 
         if (!isset($email)) {
-<<<<<<< HEAD
             return $this->json(
                 [
                     'success' => false,
                     'message' => $translation->trans('error.variable.empty')
-=======
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'message' => 'Empty required variable'
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_NOT_FOUND
             );
         }
 
-<<<<<<< HEAD
         $errorCount = $validator->validate($email, new Assert\Email([]));
 
         if (0 < count($errorCount)) {
@@ -114,13 +97,6 @@ class UserController extends Controller
                 [
                     'success' => false,
                     'message' => $translation->trans('error.email.bad')
-=======
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'message' => 'Your email contains special chars'
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_BAD_REQUEST
             );
@@ -130,27 +106,16 @@ class UserController extends Controller
         $findEmail = $entityManager->findBy(['email' => $email]);
 
         if ($findEmail) {
-<<<<<<< HEAD
             return $this->json(
                 [
                     'success' => false,
                     'message' => $translation->trans('error.record.exists')
-=======
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'message' => 'This email exists in our db.'
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_FOUND
             );
         }
-
-<<<<<<< HEAD
+        
         return $this->json(
-=======
-        return new JsonResponse(
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
             [
                 'success' => true
             ],
@@ -159,7 +124,6 @@ class UserController extends Controller
     }
 
     /**
-<<<<<<< HEAD
      * @param Request $request
      * @param TranslatorInterface $translation
      * @param ValidatorInterface $validator
@@ -172,26 +136,14 @@ class UserController extends Controller
         TranslatorInterface $translation,
         ValidatorInterface $validator
     ): JsonResponse
-=======
-     * Check if login is registered
-     */
-    public function checkLogin(Request $request): JsonResponse
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
     {
         $login = $request->query->get('login');
 
         if (!isset($login)) {
-<<<<<<< HEAD
             return $this->json(
                 [
                     'success' => false,
                     'message' => $translation->trans('error.variable.empty')
-=======
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'message' => 'Empty required variable'
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_NOT_FOUND
             );
@@ -201,27 +153,16 @@ class UserController extends Controller
         $findLogin = $entityManager->findBy(['username' => $login]);
 
         if ($findLogin) {
-<<<<<<< HEAD
             return $this->json(
                 [
                     'success' => false,
                     'message' => $translation->trans('error.record.exists')
-=======
-            return new JsonResponse(
-                [
-                    'success' => false,
-                    'message' => 'This login exists in our db.'
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_FOUND
             );
         }
 
-<<<<<<< HEAD
         return $this->json(
-=======
-        return new JsonResponse(
->>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
             [
                 'success' => true
             ],
