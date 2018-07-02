1dc49b9a34ad5a12e58f9daf278ce528739c68e4
Mateusz <nowygracz01@gmail.com>
2018-07-02 16:24:34 +0200

    Refactor

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Swagger\Annotations as SWG;
+<<<<<<< HEAD
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+=======
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
 
 class UserController extends Controller
 {
@@ -61,27 +68,59 @@ class UserController extends Controller
     }
 
     /**
+<<<<<<< HEAD
+     * @param Request $request
+     * @param TranslatorInterface $translation
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     *
+     * Check if email is registered.
+     */
+    public function checkEmail(
+        Request $request,
+        TranslatorInterface $translation,
+        ValidatorInterface $validator
+    ): JsonResponse
+=======
      * Check if email is registered.
      */
     public function checkEmail(Request $request): JsonResponse
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
     {
         $email = $request->query->get('email');
 
         if (!isset($email)) {
+<<<<<<< HEAD
+            return $this->json(
+                [
+                    'success' => false,
+                    'message' => $translation->trans('error.variable.empty')
+=======
             return new JsonResponse(
                 [
                     'success' => false,
                     'message' => 'Empty required variable'
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_NOT_FOUND
             );
         }
 
+<<<<<<< HEAD
+        $errorCount = $validator->validate($email, new Assert\Email([]));
+
+        if (0 < count($errorCount)) {
+            return $this->json(
+                [
+                    'success' => false,
+                    'message' => $translation->trans('error.email.bad')
+=======
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             return new JsonResponse(
                 [
                     'success' => false,
                     'message' => 'Your email contains special chars'
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_BAD_REQUEST
             );
@@ -91,16 +130,27 @@ class UserController extends Controller
         $findEmail = $entityManager->findBy(['email' => $email]);
 
         if ($findEmail) {
+<<<<<<< HEAD
+            return $this->json(
+                [
+                    'success' => false,
+                    'message' => $translation->trans('error.record.exists')
+=======
             return new JsonResponse(
                 [
                     'success' => false,
                     'message' => 'This email exists in our db.'
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_FOUND
             );
         }
 
+<<<<<<< HEAD
+        return $this->json(
+=======
         return new JsonResponse(
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
             [
                 'success' => true
             ],
@@ -109,17 +159,39 @@ class UserController extends Controller
     }
 
     /**
+<<<<<<< HEAD
+     * @param Request $request
+     * @param TranslatorInterface $translation
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     *
+     * Check if login is registered
+     */
+    public function checkLogin(
+        Request $request,
+        TranslatorInterface $translation,
+        ValidatorInterface $validator
+    ): JsonResponse
+=======
      * Check if login is registered
      */
     public function checkLogin(Request $request): JsonResponse
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
     {
         $login = $request->query->get('login');
 
         if (!isset($login)) {
+<<<<<<< HEAD
+            return $this->json(
+                [
+                    'success' => false,
+                    'message' => $translation->trans('error.variable.empty')
+=======
             return new JsonResponse(
                 [
                     'success' => false,
                     'message' => 'Empty required variable'
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_NOT_FOUND
             );
@@ -129,16 +201,27 @@ class UserController extends Controller
         $findLogin = $entityManager->findBy(['username' => $login]);
 
         if ($findLogin) {
+<<<<<<< HEAD
+            return $this->json(
+                [
+                    'success' => false,
+                    'message' => $translation->trans('error.record.exists')
+=======
             return new JsonResponse(
                 [
                     'success' => false,
                     'message' => 'This login exists in our db.'
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
                 ],
                 JsonResponse::HTTP_FOUND
             );
         }
 
+<<<<<<< HEAD
+        return $this->json(
+=======
         return new JsonResponse(
+>>>>>>> a3658a373854a9c93bc392c5cfd8ec4cc8117889
             [
                 'success' => true
             ],
---
+++ src/Entity/User.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="users")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    private $plainPassword;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $registerDate;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $registerIp;
+
+    public function __construct()
+    {
+        $this->registerDate = new DateTime();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getPlainPassword(): string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    public function getRegisterDate(): DateTime
+    {
+        return $this->registerDate;
+    }
+
+    public function getRegisterIp(): string
+    {
+        return $this->registerIp;
+    }
+
+    public function setRegisterIp(string $registerIp): void
+    {
+        $this->registerIp = $registerIp;
+    }
+
+    public function getRoles(): array
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
