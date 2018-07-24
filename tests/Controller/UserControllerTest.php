63918068a296d0ab36fe7f047d90e2ca85b8b75d
Mateusz <nowygracz01@gmail.com>
2018-07-24 12:57:40 +0200

    Update test

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -10,7 +10,7 @@ class UserControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $client->request('GET', '/users/checkLogin', ['login' => 'admin']);
+        $client->request('GET', '/users/check-login', ['login' => 'admin']);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
